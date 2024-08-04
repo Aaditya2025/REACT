5b49fed4abf23d2ab2b665d643a7514c0574ecc6
aditya <adityayadav135952@gmail.com>
2024-08-04 20:20:30 +0530

    creating custom react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from './chai.jsx'
+
+function App() {
+  return (  
+    <>
+    <h1>Only one component is taken</h1>
+    <Chai/>
+    <p>Tha is why we use fragment which takes all component</p>
+    </>
+    
+  )
+}
+
+export default App
